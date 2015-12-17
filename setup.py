f48239df2e39410d00fd4b678ecad4a9b90f326e
Thomas Erlang Sloth <thomas@erlang.dk>
2015-12-17 14:46:11 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='XenBackup',
-    version='0.1.1',
+    version='0.1.2',
     author='Thomas Erlang',
     author_email='thomas@erlang.dk',
     url='https://github.com/thomaserlang/xenbackup',
